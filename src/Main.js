73079025d635c58f13c4454ec59cd1073e91ca04
Wen Zhang <zhangwenfd@gmail.com>
2018-10-01 18:30:58 -0400

    register form

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Register } from './Register';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className="main">
+        <Register />
+      </div>
+    );
+  }
+}
