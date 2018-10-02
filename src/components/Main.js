2747736d87d4bcd52d5a0b94feaeab77dc16a38f
Wen Zhang <zhangwenfd@gmail.com>
2018-10-01 22:38:48 -0400

    login form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Register } from './Register';
+import { Login } from './Login';
 
 export default class Main extends Component {
   render() {
     return (
       <div className="main">
-        <Register />
+        <Login />
+        {/* <Register/> */}
       </div>
     );
   }
