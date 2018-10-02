75f1f07bdbb3a73fc71edb1cb32f8f57d26b4a32
Wen Zhang <zhangwenfd@gmail.com>
2018-10-01 22:16:16 -0400

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
 import Main from './Main.js'
-import './App.css';
+import '../styles/App.css';
 
 export default class App extends Component {
   render() {
