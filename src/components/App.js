dc460724de8384da3ef3ee6dadc2002f00b058a2
Wen Zhang <zhangwenfd@gmail.com>
2018-10-01 23:29:14 -0400

    switch between login and register

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import '../styles/App.css';
 
 export default class App extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="App">
         <Header />
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
 import { Register } from './Register';
 import { Login } from './Login';
 
 export default class Main extends Component {
+
+  getRoot = () => {
+    return <Redirect to="login" />
+  }
+
   render() {
     return (
       <div className="main">
-        <Login />
-        {/* <Register/> */}
+        <Switch>
+          <Route exact path="/" render={this.getRoot} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route render={this.getRoot} />
+        </Switch>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </div>
     );
   }
