cc71a79b5f139951b86efd1429172778941f05b6
Wen Zhang <zhangwenfd@gmail.com>
2018-10-03 22:55:57 -0400

    tabs on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { Tabs, Button } from 'antd';
+
+const TabPane = Tabs.TabPane;
+const operations = <Button>Extra Action</Button>;
 
 export default class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <Tabs tabBarExtraContent={operations} className="main-tabs">
+        <TabPane tab="Posts" key="1">Content of Tab Pane 1</TabPane>
+        <TabPane tab="Map" key="2">Content of Tab Pane 2</TabPane>
+      </Tabs>
     )
   }
 }
