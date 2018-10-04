47c2ce4bfb825d266f36e628994f9df3541ad4be
Wen Zhang <zhangwenfd@gmail.com>
2018-10-03 23:44:44 -0400

    loading status

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { Tabs, Button } from 'antd';
-import { GEO_OPTIONS } from '../constants'
+import { Tabs, Button, Spin } from 'antd';
+import { GEO_OPTIONS, POS_KEY } from '../constants'
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export default class Home extends Component {
+  state = {
+    loadingGeoLocation: false,
+    error: '',
+  }
+
   componentDidMount() {
+    this.setState({ loadingGeoLocation: true, error: '' });
     this.getGeoLocation();
   }
 
@@ -24,16 +30,33 @@ export default class Home extends Component {
 
   onSuccessLoadGeoLocation = (position) => {
     console.log(position);
+    this.setState({ loadingGeoLocation: false, err: '' });
+    const { latitude, longitude } = position.coords;
+    localStorage.setItem(POS_KEY, JSON.stringify({ lat: latitude, lon: longitude }));
   }
 
   onFailedLoadGeolocation = (error) => {
     console.log(error);
+    this.setState({ loadingGeoLocation: false, error: error.message })
   }
+
+  getGalleryPanelContent = () => {
+    if (this.state.error) {
+      return <div>{this.state.error}</div>;
+    } else if (this.state.loadingGeoLocation) {
+      return <Spin tip="Loading geo location..." />;
+    }
+    else {
+      return null;
+    }
+  }
+
 
   render() {
     return (
       <Tabs tabBarExtraContent={operations} className="main-tabs">
-        <TabPane tab="Posts" key="1">Content of Tab Pane 1</TabPane>
+        <TabPane tab="Posts" key="1"> {this.getGalleryPanelContent()}
+        </TabPane>
         <TabPane tab="Map" key="2">Content of Tab Pane 2</TabPane>
       </Tabs>
     )
--- src/constants.js
+++ src/constants.js
@@ -4,4 +4,5 @@ export const GEO_OPTIONS = {
   enableHighAccuracy: true,
   maximumAge: 3600000,
   timeout: 27000
-}
+};
+export const POS_KEY = 'POS_KEY';
