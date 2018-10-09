4af5caf3fa6478bb622bf496b41c9db20babead1
Wen Zhang <zhangwenfd@gmail.com>
2018-10-08 23:35:19 -0400

    button with basic form

--- src/components/CreatePostButton.js
+++ src/components/CreatePostButton.js
@@ -1,33 +1,45 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 
 export default class CreatePostButton extends React.Component {
-  // state = { visible: false }
+  state = { visible: false }
 
-  // showModal = () => {
-  //   this.setState({
-  //     visible: true,
-  //   });
-  // }
+  showModal = () => {
+    this.setState({
+      visible: true,
+      comfirmLoading: false,
+    });
+  }
 
-  // handleOk = (e) => {
-  //   console.log(e);
-  //   this.setState({
-  //     visible: false,
-  //   });
-  // }
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
 
-  // handleCancel = (e) => {
-  //   console.log(e);
-  //   this.setState({
-  //     visible: false,
-  //   });
-  // }
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
 
   render() {
     return (
       < div >
-        <Button type="primary">CreatePostButton</Button>
+        <Button type="primary" onClick={this.showModal}>CreatePostButton</Button>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          confirmLoading={this.state.confirmLoading}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
       </div >
     )
   }
