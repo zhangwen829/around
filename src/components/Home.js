e170f453b467b2d2950be92968ba484930396cff
Wen Zhang <zhangwenfd@gmail.com>
2018-10-08 23:12:42 -0400

    refactor loading posts, clear testing images

--- src/components/Home.js
+++ src/components/Home.js
@@ -51,10 +51,23 @@ export default class Home extends Component {
     } else if (this.state.loadingGeoLocation) {
       return <Spin tip="Loading geo location..." />;
     } else if (this.state.loadingPost) {
-      return <Spin tip="Loading nearby posts..." />;
+      return <Spin tip="Loading posts..." />;
+    } else if (this.state.posts && this.state.posts.length > 0) {
+      const images = this.state.posts.map((post) => {
+        return {
+          user: post.user,
+          src: post.url,
+          thumbnail: post.url,
+          thumbnailWidth: 400,
+          thumbnailHeight: 300,
+          caption: post.message,
+        }
+      });
+      return <Gallery images={images} />;
     }
+
     else {
-      return <Gallery images={imageList} />;
+      return null;
     }
   }
 
@@ -63,13 +76,20 @@ export default class Home extends Component {
     const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
     const token = localStorage.getItem(TOKEN_KEY);
     $.ajax({
-      url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
+      url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20000`,
       headers: {
         Authorization: `${AUTH_PREFIX} ${token}`
       }
-    })
-
+    }).then((response) => {
+      this.setState({ posts: response, loadingPost: false });
+      console.log(response);
+    }, (response) => {
+      this.setState({ error: response.responseText });
+    }).catch((error) => {
+      console.log(error);
+    });
   }
+
 
   render() {
     return (
@@ -81,79 +101,3 @@ export default class Home extends Component {
     )
   }
 }
-
-const imageList = [
-  {
-    user: 'wen',
-    src: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_b.jpg",
-    thumbnail: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_n.jpg",
-    thumbnailWidth: 271,
-    thumbnailHeight: 320,
-    tags: [{ value: "Nature", title: "Nature | Flowers" }],
-    caption: "Orange Macro (Tom Eversley - isorepublic.com)"
-  },
-  {
-    user: 'wen',
-    src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
-    thumbnail: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 190,
-    tags: [{ value: "Architecture", title: "Architecture | Outdoors" },
-    { value: "Industrial", title: "Industrial" }],
-    caption: "286H (gratisography.com)"
-  },
-  {
-    user: 'wen',
-    src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
-    thumbnail: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 148,
-    tags: [{ value: "People", title: "People" },
-    { value: "Architecture", title: "Architecture | Outdoors" },
-    { value: "Industrial", title: "Industrial" }],
-    caption: "315H (gratisography.com)"
-  },
-  {
-    user: 'wen',
-    src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
-    thumbnail: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 213,
-    caption: "201H (gratisography.com)"
-  },
-  {
-    user: 'wen',
-    src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
-    thumbnail: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_n.jpg",
-    thumbnailWidth: 248,
-    thumbnailHeight: 320,
-    caption: "Big Ben (Tom Eversley - isorepublic.com)"
-  },
-  {
-    user: 'wen',
-    src: "https://c1.staticflickr.com/9/8785/28687743710_870813dfde_h.jpg",
-    thumbnail: "https://c1.staticflickr.com/9/8785/28687743710_3580fcb5f0_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 113,
-    tags: [{ value: "People", title: "People" },
-    { value: "Industrial", title: "Industrial" }],
-    caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)"
-  },
-  {
-    user: 'wen',
-    src: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
-    thumbnail: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_n.jpg",
-    thumbnailWidth: 313,
-    thumbnailHeight: 320,
-    caption: "Wood Glass (Tom Eversley - isorepublic.com)"
-  },
-  {
-    user: 'wen',
-    src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
-    thumbnail: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 213,
-    tags: [{ value: "Nature", title: "Nature | Flowers" }],
-    caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)"
-  }
-];
