4a87653b5e62322609c589ba6540ea33fc4de6e2
Wen Zhang <zhangwenfd@gmail.com>
2019-06-26 18:36:00 -0400

    show info window on condition, add toggleOpen

--- src/components/AroundMarker.js
+++ src/components/AroundMarker.js
@@ -2,16 +2,34 @@ import React from 'react';
 import { Marker, InfoWindow } from 'react-google-maps';
 
 export class AroundMarker extends React.Component {
+  state = {
+    isOpen: false
+  }
+
+  onToggleOpen = () => {
+    this.setState((prevState) => ({
+      isOpen: !prevState.isOpen
+    }))
+  }
+
   render() {
     const { location, user, message, url } = this.props.post;
     return (
-      <Marker position={{ lat: location.lat, lng: location.lon }}>
-        <InfoWindow>
-          <div>
-            <img className="around-marker-image" src={url} />
-            <p>{`${user}: ${message}`}</p>
-          </div>
-        </InfoWindow>
+      <Marker
+        position={{ lat: location.lat, lng: location.lon }}
+        onMouseOver={this.onToggleOpen}
+        onMouseOut={this.onToggleOpen}
+      >
+        {
+          this.state.isOpen ? (
+            <InfoWindow onCloseClick={this.onToggleOpen}>
+              <div>
+                <img className="around-marker-image" src={url} />
+                <p>{`${user}: ${message}`}</p>
+              </div>
+            </InfoWindow>
+          ) : null
+        }
       </Marker>
     )
   }
